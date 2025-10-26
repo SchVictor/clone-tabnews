6058fadcd07d77d3220742c1603006033ae40ec1
Victor Schiessl <147779582+SchVictor@users.noreply.github.com>
2025-10-26 17:56:27 +0000

    adiciona scripts `lint:check` e `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>white</h1>
+function Home() {
+  return <h1>white</h1>;
 }
+
+function TextDecoderStream() {
+  console.log("treste");
+}
+
 export default Home;
-  
